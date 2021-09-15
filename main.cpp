ab37e58b330101d6c48e32298cc22bbf791a85db
S0ftRunner <benjamin.sokolov19@gmail.com>
2021-09-15 17:07:40 +0300

    first commit

---
+++ main.cpp
@@ -0,0 +1,70 @@
+#include <iostream>
+#include <cmath>
+using namespace std;
+//----------------------
+int ConvertBinaryToDec(int number);
+int ConvertDecToBinary(int number);
+//----------------------
+int main()
+{
+    setlocale(LC_ALL, "ru"); //
+    int number; // вводимое число
+    char switcher; // переключатель
+
+    do // простое меню
+    {
+        cout << "Выберите, из какой в какую систему вы хотите перевести число:\n";
+        cout << "1. Из двоичной в десятичную\n";
+        cout << "2. Из десятичной в двоичную\n";
+        cout << "Для выхода нажмите 'q'\n\n";
+        cin >> switcher;
+        switch(switcher)
+        {
+            case '1' :
+                cout << "Введите число: ";
+                cin >> number;
+                cout << number << " (binary) = " << ConvertBinaryToDec(number) << " (dec) \n\n";
+                break;
+            case '2' :
+                cout << "Введите число: ";
+                cin >> number;
+                cout << number << " (dec) = " << ConvertDecToBinary(number) << " (binary) \n\n";
+                break;
+        }
+
+    }while (switcher != 'q'); // если пользователь ввел q, то программа завершается
+
+    return 0;
+}
+//----------------------
+int ConvertBinaryToDec(int number)
+{
+    int DecimalNumber = 0, i = 0, remainder;
+
+    while (number != 0)
+    {
+        remainder = number % 10; // берем число справа
+        number /= 10; // "укорачиваем" число справа
+        DecimalNumber += remainder * pow(2, i); // умножаем первое число справа на двойку в степени шага
+        ++i; // счетчик повышается
+    }
+
+    return DecimalNumber;
+}
+//----------------------
+int ConvertDecToBinary(int number)
+{
+    int BinaryNumber = 0;
+    int remainder, i = 1, step = 1;
+
+    while (number != 0)
+    {
+        remainder = number % 2;
+        cout << "Шаг: " << step++ << ' ' << number << "/2, Остаток = " << remainder << ", Quotient = " << number /2 << "\n";
+        number /= 2;
+        BinaryNumber += remainder * i;
+        i *= 10;
+    }
+
+    return BinaryNumber;
+}
